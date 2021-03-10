e2295b5fcfb317c96763a2da24f09472fc065633
Jake Meyers <jakejake92@yahoo.com>
2021-03-09 20:02:02 -0700

    made component for data schema

---
+++ src/context/Data.js
@@ -0,0 +1,20 @@
+const data = [
+	{
+		company: "Xcel Energy",
+		jobs: [
+			{
+				Buyer: {
+                    {
+                        salary: 54000, 
+                        bonus: 3000, 
+                        PTO: 17,
+                        HealthCare: "Great high deductable plan. Pay 30 dollars per month.",
+                        Management: "I'm in my second job with Xcel. My current manager is much better than the last. It's pretty hit or miss", 
+                        Other: "Overall a great place to work. Low stress, decent pay",
+                        stars: 4.5
+                    }
+				},
+			},
+		],
+	},
+];
