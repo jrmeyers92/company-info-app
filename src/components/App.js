e5411ec469c64542e4fcdbdc12a2759154a75b2b
Jake Meyers <jakejake92@yahoo.com>
2021-03-09 19:33:21 -0700

    project init and folder setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Route from "./route/Route";
+
+function App() {
+	return (
+		<Router>
+			<Route />
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/components/route/Route.js
@@ -0,0 +1,9 @@
+import React from "react";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+const Route = () => {
+	return <div></div>;
+};
+
+export default Route;
